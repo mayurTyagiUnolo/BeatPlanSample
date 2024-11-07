e25dd19f01bd775441431b8c8d1509d18e7432f7
mayurTyagiUnolo <115685636+mayurTyagiUnolo@users.noreply.github.com>
2024-11-07 11:18:34 +0530

    Initial Commit

---
+++ BeatPlan/BeatPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatPlanApp.swift
+//  BeatPlan
+//
+//  Created by Mayur Tyagi on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BeatPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
