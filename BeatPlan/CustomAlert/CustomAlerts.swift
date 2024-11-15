5f67addb949d3f47dcbef617385dbd7b4738ad4e
mayurTyagiUnolo <115685636+mayurTyagiUnolo@users.noreply.github.com>
2024-11-15 10:23:56 +0530

    a

--- BeatPlan/CustomAlert/CustomAlerts.swift
+++ BeatPlan/CustomAlert/CustomAlerts.swift
@@ -11,26 +11,37 @@ import SwiftUI
 struct CustomAlertOfOptions: View {
     var optionOne: String
     var optionSecond: String
+    @State private var optionOneSelected: Bool = true
+    @State private var optionSecondSelected: Bool = false
+    
     @Binding var showAlert: Bool
     
     var body: some View {
         VStack(alignment: .leading){
             VStack(alignment: .leading, spacing: 30){
                 Button{
-                    
+                    optionOneSelected.toggle()
+                    optionSecondSelected.toggle()
                 } label: {
                     HStack{
-                        Image(systemName: "circle")
+                        Image(systemName: optionOneSelected ? "record.circle" : "circle")
+                            .foregroundStyle(optionOneSelected ? .blue : .gray)
+                            .font(.title)
                         Text(optionOne)
+                            .foregroundStyle(.black)
                     }
                 }
                 
                 Button{
-                    
+                    optionOneSelected.toggle()
+                    optionSecondSelected.toggle()
                 } label: {
                     HStack{
-                        Image(systemName: "circle")
+                        Image(systemName: optionSecondSelected ? "record.circle" : "circle")
+                            .font(.caption)
+                            .foregroundStyle(optionSecondSelected ? .blue : .gray)
                         Text(optionSecond)
+                            .foregroundStyle(.black)
                     }
                 }
             }
@@ -40,9 +51,10 @@ struct CustomAlertOfOptions: View {
             HStack{
                 Spacer()
                 
-                Button("cancel"){
+                Button("Cancel"){
                     showAlert = false
                 }
+                .foregroundStyle(.gray)
                 .padding()
                 
                 Button("Apply"){
@@ -70,32 +82,36 @@ struct CustomAlertOfTextView: View {
             Text("Remarks")
                 .padding()
             
-            TextField("Comment", text: $text, prompt: Text("Please input your comment"), axis: .vertical)
+            TextField("Comment", text: $text, prompt: Text("Write your comment here..."), axis: .vertical)
                 .lineLimit(6...6)
                 .frame(maxWidth: .infinity, maxHeight: 150)
-                .padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 6))
+                .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10))
                 .overlay {
                     RoundedRectangle(cornerRadius: 5)
                         .stroke(SwiftUI.Color.gray.opacity(0.5), lineWidth: 0.5)
                 }
                 .foregroundStyle(.secondary)
-                .background(.gray.opacity(0.08))
+                .background(.gray.opacity(0.05))
                 .padding(.horizontal)
             
-            HStack(spacing: 10){
-                Button("cancel"){
+            HStack(spacing: 20){
+                Button("Go Back"){
+                    showAlert = false
+                }
+                .frame(maxWidth: .infinity)
+                .padding(EdgeInsets(top: 13, leading: 0, bottom: 13, trailing: 0))
+                .background(.gray.opacity(0.2))
+                .foregroundStyle(.black)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                Button("Cancel Visit"){
                     showAlert = false
                 }
                 .frame(maxWidth: .infinity)
                 .padding(EdgeInsets(top: 13, leading: 0, bottom: 13, trailing: 0))
                 .background(.red)
-                
-                Button("Apply"){
-                    showAlert = false
-                }
-                .frame(maxWidth: .infinity)
-                .padding(EdgeInsets(top: 13, leading: 0, bottom: 13, trailing: 0))
-                .background(.green)
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
             }
             .frame(maxWidth: .infinity)
             .padding()
@@ -108,5 +124,5 @@ struct CustomAlertOfTextView: View {
 }
 
 #Preview{
-    CustomAlertOfTextView(showAlert: .constant(true))
+    CustomAlertOfOptions(optionOne: "change only today's plan", optionSecond: "change all scheduled plan", showAlert: (.constant(true)))
 }
